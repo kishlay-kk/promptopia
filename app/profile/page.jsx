bd7a48e6394e92995aa5e730b1c18a8869402f46
Kishlay Kishore <kishlaythebeatmaker@gmail.com>
2023-10-21 17:14:32 +0530

    Create posts and  MyProfile page

---
+++ app/profile/page.jsx
@@ -0,0 +1,44 @@
+'use client';
+import { useState, useEffect} from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Profile from '@components/profile';
+
+const MyProfile = () => {
+    const router = useRouter();
+    const {data: session} = useSession();     //const {data: session} means const data renamed to session
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        //Fetch data from server
+        const fetchPosts = async () => {
+          const response = await fetch(`/api/users/${session?.user.id}/posts`);
+          const data = await response.json();
+          setPosts(data);
+        }
+    
+        if(session?.user.id)
+            fetchPosts();
+      }, []);
+
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`)
+    }
+
+    const handleDelete = async (post) => {
+
+    }
+  return (
+    <Profile 
+        name ="My"
+        desc = "Welcome to you personalized profile page"
+        data = {posts}
+        handleEdit = {handleEdit}
+        handleDelete = {handleDelete}
+    />
+  )
+}
+
+export default MyProfile
